7a9da1aab2feb4003ef21d4ade148d962ba21a10
ArunReddy <rakasiarunkumarreddy@gmail.com>
2024-08-21 02:00:22 +0530

    src

---
+++ src/App.js
@@ -0,0 +1,62 @@
+// import React from 'react';
+// import Dashboard from './components/Dashboard';
+// import './App.css';
+
+// const App = () => (
+//   <div>
+//     <h1>Dynamic Dashboard</h1>
+//     <Dashboard />
+//   </div>
+// );
+
+// export default App;
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import Dashboard from './components/Dashboard';
+
+// const App = () => {
+//   const appStyle = {
+//     textAlign: 'center',
+//     color: '#333'
+//   };
+
+//   return (
+//     <div>
+//       <h1 style={appStyle}>Dynamic Dashboard</h1>
+//       <Dashboard />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
+
+
+
+import React from 'react';
+import Dashboard from './components/Dashboard';
+
+const App = () => {
+  const appStyle = {
+    textAlign: 'center',
+    color: '#333'
+  };
+
+  return (
+    <div>
+      <h1 style={appStyle}>Dynamic Dashboard</h1>
+      <Dashboard />
+    </div>
+  );
+};
+
+export default App;
